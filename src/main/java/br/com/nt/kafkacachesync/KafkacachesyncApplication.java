332c1b0c07cf734ecfc99f423110faea1a619369
Ediwaldo Neto <ediwaldo.neto@outlook.com>
2024-08-27 21:49:42 -0300

    inicio projeto

---
+++ src/main/java/br/com/nt/kafkacachesync/KafkacachesyncApplication.java
@@ -0,0 +1,13 @@
+package br.com.nt.kafkacachesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkacachesyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkacachesyncApplication.class, args);
+	}
+
+}
